0d4ea2976ab862ffb135678e09ca07580317b3da
Dandi2k8 <drd.construction30@gmail.com>
2026-02-05 17:52:56 -0800

    Fixing the Javadoc

--- src/main/java/com/drd/backported/item/SpearItem.java
+++ src/main/java/com/drd/backported/item/SpearItem.java
@@ -1,12 +1,11 @@
 package com.drd.backported.item;
 
-/**
- * Just so you know, making the spears is still a heavy WIP, so it will take a while to get the spear item models and the spear itself to work
- */
-
 import net.minecraft.world.item.*;
 
 public class SpearItem extends TieredItem {
+    /**
+     * Just so you know, making the spears is still a heavy WIP, so it will take a while to get the spear item models and the spear itself to work.
+     */
     public SpearItem(Tier tier, Properties properties) {
         super(tier, properties);
     }
